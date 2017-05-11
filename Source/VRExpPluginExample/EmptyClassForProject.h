d7c5a849b9957a33bb2afd88e2fe24d2b3636bdf
JamesNye <iamthemostawesomestpersonever@gmail.com>
2017-05-10 22:04:15 -0500

    Initial commit

---
+++ Source/VRExpPluginExample/EmptyClassForProject.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "VRCharacter.h"
+#include "GripMotionControllerComponent.h"
+#include "VRBPDatatypes.h"
+#include "VRGripInterface.h"
+#include "VRSimpleCharacter.h"
+#include "VRPlayerController.h"	
+#include "VRExpansionFunctionLibrary.h"
+#include "GrippableBoxComponent.h"
+#include "VRStereoWidgetComponent.h"
+#include "VRCharacterMovementComponent.h"
+
+/**
+ * 
+ */
+class VREXPPLUGINEXAMPLE_API EmptyClassForProject
+{
+public:
+	EmptyClassForProject();
+	~EmptyClassForProject();
+
+};
